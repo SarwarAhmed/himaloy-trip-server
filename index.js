0c5f3eba19ade9253f28d0a66d7ba4e8249c5300
Sarwar Ahmed <sarwar.amas@gmail.com>
2024-04-30 08:58:20 +0600

    Get the single spot

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // const uri = "mongodb+srv://<username>:<password>@cluster0.5mrfovz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5mrfovz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -43,6 +43,13 @@ async function run() {
             const cursor = spostCollection.find({});
             const sposts = await cursor.toArray();
             res.send(sposts);
+        });
+
+        app.get('/spot/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const spost = await spostCollection.findOne(query);
+            res.send(spost);
         });
 
 
